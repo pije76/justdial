491cd8a5b1e802f881a28aaa5043149a2bb0445d
Panjianom <panjul76@hotmail.com>
2020-04-10 00:37:09 +0700

    allinone

--- listing/views.py
+++ listing/views.py
@@ -100,37 +100,6 @@ def listing_detail(request, country_id, region_id, city_id, category_slug, slug)
     return render(request, 'view_ad.html', context)
 
 
-def us_cities(request, slug):
-    us_cities = City.objects.filter(country__id='234')
-    context = {
-        'us_cities': us_cities,
-    }
-    return render(request, "us_cities.html", context)
-
-
-def asia_cities(request, slug):
-    canada_cities = City.objects.filter(country__id='38')
-    uk_cities = City.objects.filter(country__id='77')
-    australia_cities = City.objects.filter(continent='OC')
-    africa_cities = City.objects.filter(continent='AF')
-    asia_cities = City.objects.filter(continent='AS')
-    middle_cities = City.objects.filter(continent='')
-    europe_cities = City.objects.filter(continent="EU")
-    latin_cities = City.objects.filter(continent='SA')
-    context = {
-        'us_cities': us_cities,
-        'canada_cities': canada_cities,
-        'uk_cities': uk_cities,
-        'australia_cities': australia_cities,
-        'africa_cities': africa_cities,
-        'asia_cities': asia_cities,
-        'middle_cities': middle_cities,
-        'europe_cities': europe_cities,
-        'latin_cities': latin_cities,
-    }
-    return render(request, "asia_cities.html", context)
-
-
 def category(request):
     #    parentcategory = Category.objects.add_related_count(Category.objects.all(), Project, 'category', 'cat_count', cumulative=True)
     categories = Category.objects.add_related_count(Category.objects.root_nodes(), Listing, 'category', 'cat_count', cumulative=True)
@@ -139,40 +108,3 @@ def category(request):
     }
     return render(request, 'category.html', context)
 
-
-class RegionList(ListView):
-    model = Region
-    template_name = "us_region.html"
-
-    def get_queryset(self, **kwargs):
-        self.region = get_object_or_404(Region, slug=self.kwargs['region_slug'])
-        return City.objects.filter(region=self.region)
-
-    def get_context_data(self, **kwargs):
-        context = super(RegionList, self).get_context_data(**kwargs)
-        context['section'] = 'city'
-        return context
-
-
-class CityList(ListView):
-    model = City
-    template_name = "us_region.html"
-    ordering = ('name')
-
-    def places(self, **kwargs):
-        country = get_object_or_404(Country, slug=self.kwargs['country_slug'])
-        region = get_object_or_404(Region, slug=self.kwargs['region_slug'])
-        city = get_object_or_404(City, slug=self.kwargs['city_slug'])
-        return country, region, city
-
-    def get_queryset(self, **kwargs):
-        self.country = get_object_or_404(Country, slug=self.kwargs['country_slug'])
-        self.region = get_object_or_404(Region, slug=self.kwargs['region_slug'])
-        self.city = get_object_or_404(City, slug=self.kwargs['city_slug'])
-        return City.objects.filter(country=self.country, region=self.region, chool_city=self.city)
-
-    def get_context_data(self, **kwargs):
-        context = super(CityList, self).get_context_data(**kwargs)
-        context['section'] = 'city'
-        context['country'] = get_object_or_404(Country, slug=self.kwargs['country_slug'])
-        return context
